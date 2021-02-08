5b6dcb9fa79bb6d855c69f28a33814f5fdb82fa2
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-07 23:35:47 -0300

    feat: got cube working and animated. fuck yessssss

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+const Cube = () => {
+  const meshRef = useRef();
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    color: hovered ? "hotpink" : "gray",
+  });
+  useFrame(() => {
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <a.mesh
+      ref={meshRef}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach="material" color={props.color} />
+    </a.mesh>
+  );
+};
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-full">
+      <Canvas className="w-full min-h-screen">
+        <Cube />
+      </Canvas>
+    </div>
+  );
+}
+
+import { useState, useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { useSpring, a } from "@react-spring/three";
